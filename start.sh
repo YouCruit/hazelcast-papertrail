cbfa511c89f081ef8b8380e45a520eb8de91dd30
Snōwball <tobias@olsson.be>
2019-02-25 16:29:56 +0100

    Add sed command

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ papertrailHost="${PAPERTRAIL%%:*}"
 papertrailPort="${PAPERTRAIL##*:}"
 
 echo "Logging to $papertrailHost:$papertrailPort"
-sed "s/PAPERTRAIL_HOST/$papertrailHost/g" "s/PAPERTRAIL_PORT/$papertrailPort/g" -i /etc/rsyslog.conf
+sed -e "s/PAPERTRAIL_HOST/$papertrailHost/g" -e "s/PAPERTRAIL_PORT/$papertrailPort/g" -i /etc/rsyslog.conf
 
 /usr/sbin/rsyslogd
 
